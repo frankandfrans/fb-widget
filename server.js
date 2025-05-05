d2b96b2946d18bb85532fad4e0c0b93386f848e3
frankandfrans <frankandfransavon@gmail.com>
2025-05-05 12:11:05 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const path = require('path');
 
 const PAGE_ID = '210175288809';
-const ACCESS_TOKEN = 'EAAUHRrIZCMu8BO2ZCB9hmKb6iJfis22wnmH0ySeUHTE1TdmQTkxavxZCgs22iXmItj583cxVxlf8mLucOJhMkaAoZCZCizikrsO4gAH1dVsTeh5mzwHt5NP9yoX7MT0Dl4lEj4t2O3cQbQ2ZArKKAZBgyYO0M82DvZAEvSHkZCji721VHhoT39fmMRsEs9rIZD';
+const ACCESS_TOKEN = 'REPLACE_WITH_YOUR_VALID_PAGE_ACCESS_TOKEN';
 
 const app = express();
 
@@ -18,7 +18,6 @@ app.use(express.static(__dirname));
 app.get('/fb-posts', async (req, res) => {
   try {
     const url = `https://graph.facebook.com/v22.0/${PAGE_ID}/posts?fields=message,attachments{subattachments{media},media}&limit=10&access_token=${ACCESS_TOKEN}`;
-
     const fbRes = await fetch(url);
 
     if (!fbRes.ok) {
